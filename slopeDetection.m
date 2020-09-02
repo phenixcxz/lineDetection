3e3f10946813f14dd44e4721ae951317b941b23c
cxz <vcx@cxzs-MacBook-Air.local>
2020-09-03 02:16:29 +0800

    优化程序，修复bug

--- slopeDetection.m
+++ slopeDetection.m
@@ -1,7 +1,7 @@
 function [slope,slValueLeft,slValueRight,slopeX,slopeY]=slopeDetection(dirlist_lineT,dirlistT_lens,img,M,N,Msize)
 %% 直线拟合+信息统计
 % imgdown = zeros(M+2*Msize,N+2*Msize);
-%斜率，中点，连续点数
+%斜率，起始点，连续点数，计算的起点，图像中点，
 slope=zeros(dirlistT_lens,5);       %斜率计算
 for m=1:dirlistT_lens
     aa=dirlist_lineT{m};  %待处理线段
@@ -9,15 +9,14 @@ for m=1:dirlistT_lens
     p = polyfit(aa(round(x*2/10):round(x*8/10),1),aa(round(x*2/10):round(x*8/10),2),1);     %线段斜率与起始点
     slope(m,1) = atan(p(1));
     if slope(m,1) < 0
-        slope(m,1) = 3.1415+slope(m,1);
+        slope(m,1) = pi+slope(m,1);
     end
     slope(m,2) = round(p(2));       %起始点
-%     slope(m,5) = round(p(2));
     slope(m,5) = round(p(1)*(M/2+Msize)+p(2));      %中点
     slope(m,3) = x;                 %连续点个数
+    slope(m,4) = aa(1,1);
     
 end
-
 
 %% 斜率排序
 slope2 = slope;
@@ -26,89 +25,38 @@ slope2(:,3) = slope(pos,3);
 slope2(:,2) = slope(pos,2);
 slope2(:,1)= slope(pos,1);
 
+gap=0.03;
+gaplen = ceil(pi/gap);
 
-%% 寻找适合斜率
-% [slsize,y] = size(slope);
-slsize = length(slope);
-dotNum = 20;
-figure(21)
-slHist = histogram(slope2(slsize-dotNum+1:slsize,1),6);   %斜率直方图
-
-slValue = slHist.Values;
-slEdge = slHist.BinEdges;
-[maxSl,ord] = max(slValue);
-BinLimits = slHist.BinLimits(2) - slHist.BinLimits(1);
-if BinLimits<0.5
-   slEdgeLeft =  slEdge(1);
-   slEdgeRight = slEdge(7);
-elseif maxSl > dotNum/2
-    slEdgeLeft = slEdge(ord);
-    slEdgeRight = slEdge(ord+1);
-else
-    
-
-    slEdgeLeft = 0;
-    slEdgeRight = 0;
-
-    for i = 1:length(slValue)
-        if slValue(i) >= dotNum/4
-            slEdgeLeft = slEdge(i);
-            break;
-        end
-    end
-    for i = length(slValue):-1:1
-        if slValue(i) >= dotNum/5
-            slEdgeRight = slEdge(i+1);
-            break;
-        end
+slopeR = zeros(1,gaplen);
+for m = 1:length(slope2)
+    num = ceil(slope2(m,1)/gap);
+    slopeR(num) = slopeR(num)+1;
 end
+slopeR2 = zeros(1,gaplen);
+for m = 2:gaplen-1
+    slopeR2(m) = slopeR(m-1)+slopeR(m)+slopeR(m+1);
 end
+slopeR2(1) = slopeR(1)+slopeR(2)+slopeR(gaplen);
+slopeR2(gaplen) = slopeR(1)+slopeR(gaplen-1)+slopeR(gaplen);
 
+[~,maxDot]=max(slopeR2(:));
 flag1 = 0;
-if slEdgeRight-slEdgeLeft > 2
-    for i = 1:length(slValue)
-        if slValue(i) >= dotNum/5
-            slEdgeRight = slEdge(i+1);
-            break;
-        end
-    end
-    for i = length(slValue):-1:1
-        if slValue(i) >= dotNum/5
-            slEdgeLeft = slEdge(i);
-            break;
-        end
-    end 
-    flag1 =1;
-    meanNum1 = 0;
-    meanValue1 = 0;
-    meanNum2 = 0;
-    meanValue2 = 0;
-    for i = slsize-dotNum+1:slsize
-        if slope2(i,1)>slEdgeLeft 
-            meanNum1 = meanNum1+1;
-            meanValue1 = meanValue1 + slope2(i,1);
-        elseif slope2(i,1) < slEdgeRight
-            meanNum2 = meanNum2+1;
-            meanValue2 = meanValue2 + slope2(i,1);
-        end
-    end
-    slValueLeft = meanValue1/meanNum1-0.03; %%斜率边界    
-    slValueRight = meanValue2/meanNum2+0.03;    
-    
+slValueLeft = 0;
+slValueRight = 0;
+if maxDot ==1 
+    slValueLeft=pi-gap;
+    slValueRight = gap*2;
+    flag1 = 1;
+elseif maxDot == gaplen
+        flag1 = 1;
+    slValueLeft=pi-gap*2;
+    slValueRight = gap;
 else
-    meanValue = 0;
-    meanNum = 0;
-    for i = slsize-dotNum+1:slsize
-        if slope2(i,1)>slEdgeLeft && slope2(i,1) < slEdgeRight
-            meanNum = meanNum+1;
-            meanValue = meanValue + slope2(i,1);
-        end
-    end
-    slValueLeft = meanValue/meanNum-0.03; %%斜率边界
-    slValueRight = meanValue/meanNum+0.03; 
+    slValueLeft = gap*(maxDot-2);
+    slValueRight = gap*(maxDot+1);   
 end
-
-
+    
 %% 弧度约束
 slopeX = zeros(dirlistT_lens,1);
 pi1 = pi/4;
@@ -131,7 +79,7 @@ for m = 1:dirlistT_lens
         end
     end
 end
-
+% 
 % imgSlope = zeros(M+2*Msize,N+2*Msize);
 % for m = 1:dirlistT_lens
 %     if slopeX(m,1) == 1
@@ -159,33 +107,6 @@ end
 %     end
 % end
 % figure('Name','斜率约束'),imshow(imgSlope);
-% %% 延长线
-% imgSlope = zeros(M+2*Msize,N+2*Msize);
-% for m = 1:dirlistT_lens
-%     if slopeX(m,1) == 1
-%         aa = dirlist_lineT{m};
-%         aaMax = max(aa(:,1));
-%         aaMin = min(aa(:,1));
-%         for n = Msize:M+Msize
-%             xx = n;
-%             yy = round(n*slope(m,1)+slope(m,2));
-%             if yy < N+2*Msize && yy >= 1
-%                 imgSlope(xx,yy) = 255;
-%             end
-%         end
-%     elseif slopeY(m,1) == 1 
-%         aa = dirlist_lineT{m};        
-%         aaMax = max(aa(:,2));
-%         aaMin = min(aa(:,2));
-%         for n = Msize:N+Msize
-%             yy = n;
-%             xx = round((yy-slope(m,2))/slope(m,1));
-%             if xx < M+2*Msize && xx >= 1
-%                 imgSlope(xx,yy) = 255;
-%             end
-%         end 
-%     end
-% end
-% figure('Name','斜率约束'),imshow(imgSlope);
+
 
 end
